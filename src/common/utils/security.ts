46f2ce9b8f38356fe194924ac00241c28e45c5f2
LuongLegend <luongprolv99@gmail.com>
2026-09-06 23:38:36 +0700

    feat: rm ca-cert from git

--- src/common/utils/security.ts
+++ src/common/utils/security.ts
@@ -1,10 +1,29 @@
 import path from 'path';
 import fs from 'fs';
+import * as dotenv from 'dotenv';
+
+dotenv.config({
+  path: '.env',
+});
 
 const CERTS_DIR = path.join(process.cwd(), 'certs');
+const CA_CERT_BASE64 = process.env.CA_CERT_BASE64;
 
 export const getCACertificate = () => {
   const caPath = path.join(CERTS_DIR, 'ca-certificate.crt');
+
+  if (CA_CERT_BASE64) {
+    //chưa có folder certs thì tạo mới
+    if (!fs.existsSync(CERTS_DIR)) {
+      fs.mkdirSync(CERTS_DIR, { recursive: true });
+    }
+
+    //decode base64
+    const certBuffer = Buffer.from(CA_CERT_BASE64, 'base64');
+
+    fs.writeFileSync(caPath, certBuffer);
+  }
+
   if (!fs.existsSync(caPath)) {
     throw new Error(`CA certificate not found at ${caPath}`);
   }
